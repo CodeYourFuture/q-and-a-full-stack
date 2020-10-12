ecc68df2201e0c79afc12bdd920ddb1c9a2c8a2b
Osman Elsahib <elsahib10@gmail.com>
2020-10-12 19:11:12 +0100

    trying postgress connection

--- server/api.js
+++ server/api.js
@@ -6,12 +6,22 @@ import { Connection } from "./db";
 const router = new Router();
 
 router.get("/", (_, res, next) => {
-	
+
 	Connection.connect((err) => {
 		if (err) {
 			return next(err);
 		}
-		res.json({ message: "Hello, world!" });
+		res.json({ message: "Hello, osman!" });
+	});
+});
+
+router.get("/test", (_, res, next) => {
+
+	Connection.query("select * from questions", (err,result) => {
+		if (err) {
+			return next(err);
+		}
+		res.json({ message:result.rows });
 	});
 });
 
