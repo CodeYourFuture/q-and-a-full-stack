b8726d7b4bcdc313a72ddd4297719979a153ad95
halit <oskan96@gmail.com>
2020-10-20 19:46:42 +0100

    removed unused lines

--- client/src/Components/SingleQuestion.js
+++ client/src/Components/SingleQuestion.js
@@ -2,20 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export default function SingleQuestion({ context }) {
-  console.log(context);
-  return (
-    // <div className="container">Single Question Display Under Construction</div>
-    <>
-      <div>{context}</div>
-      {/* <ShowContext
-        postComment={postComment}
-        getComments={getComments}
-        id={id}
-        title={title}
-        context={context}
-      /> */}
-    </>
-  );
+  return <div>{context}</div>;
 }
 
 SingleQuestion.propTypes = {
