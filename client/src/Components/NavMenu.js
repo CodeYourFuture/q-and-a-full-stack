42d7435ec7aa56da8be72c2920688f7fe038a85f
Laetitia <laetitiachira@gmail.com>
2020-10-20 14:03:45 +0100

    added welcome to app section

--- client/src/Components/NavMenu.js
+++ client/src/Components/NavMenu.js
@@ -5,26 +5,48 @@ import { Link } from "react-router-dom";
 
 const NavMenu = () => {
   return (
-    <Container>
-      <Navbar
-        expand="lg"
-        variant="dark"
-        bg="secondary"
-        className="justify-content-around p-2 my-2"
-      >
-        <Link to="/">
-          <button type="button" className="btn btn-success">
-            All Questions
-          </button>
-        </Link>
-        <Navbar.Text href="#" className="text-white">
-          <h2 className="display-4">The Gradutes Q&A</h2>
-        </Navbar.Text>
-        <Link to="/ask" type="button" className="btn btn-info m-3">
-          Ask a question
-        </Link>
-      </Navbar>
-    </Container>
+    <div>
+      <Container>
+        <Navbar
+          expand="lg"
+          variant="dark"
+          bg="secondary"
+          className="justify-content-around p-2 my-2"
+        >
+          {/* <Link to="/">
+            <button type="button" className="btn btn-success">
+              All Questions
+            </button>
+          </Link> */}
+          <Navbar.Text href="#" className="text-white">
+            <h2 className="display-4">The Graduates</h2>
+          </Navbar.Text>
+          {/* <Link to="/ask" type="button" className="btn btn-info m-3">
+            Ask a question
+          </Link> */}
+        </Navbar>
+      </Container>
+      <Container>
+        <div class="WelcomeSection">
+          <h1>Welcome to the Q&A app</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries,
+          </p>
+          <Link to="/">
+            <button type="button" className="btn btn-success">
+              All Questions
+            </button>
+          </Link>
+          <Link to="/ask" type="button" className="btn btn-info m-3">
+            Ask a question
+          </Link>
+        </div>
+      </Container>
+    </div>
   );
 };
 
