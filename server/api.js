b95d8bd86d0ef696cda95dd6b6e80e7044393f7e
Osman Elsahib <elsahib10@gmail.com>
2020-11-01 18:30:47 +0000

    Added put and delete end points

--- server/api.js
+++ server/api.js
@@ -69,6 +69,7 @@ router.post("/question", (req, res, next) => {
     context = req.body.context,
     email = req.body.email,
     token = req.body.token;
+  console.log(token);
   admin
     .auth()
     .verifyIdToken(token)
@@ -130,4 +131,159 @@ router.post("/newuser", (req, res, next) => {
     });
 });
 
+router.put("/question", (req, res, next) => {
+  let questionId = req.body.id,
+    title = req.body.title,
+    context = req.body.context,
+    email = req.body.email,
+    token = req.body.token;
+  let checksql = "select * from questions where id = $1";
+  let updatesql =
+    "update questions set title=$1,context=$2 where id =$3 returning *";
+  admin
+    .auth()
+    .verifyIdToken(token)
+    .then(() => {
+      return Connection.query(checksql, [questionId]).then((result) => {
+        if (result.rowCount > 0) {
+          if (result.rows[0].email !== email) {
+            return "You are not the author of this question!";
+          }
+          return Connection.query(updatesql, [title, context, questionId]);
+        } else {
+          return null;
+        }
+      });
+    })
+    .then((result) => {
+      result
+        ? result.rows
+          ? res.status(200).json(result.rows)
+          : res.status(200).json({ message: result })
+        : res.status(404).json({ message: "There is no such question!" });
+    })
+    .catch((e) => {
+      if (e) {
+        console.log(e.stack);
+        next(e);
+      } else {
+        res.send({ message: "Could not authorize" }).status(403);
+      }
+    });
+});
+
+router.put("/comment", (req, res, next) => {
+  let commentId = req.body.id,
+    comment = req.body.comment,
+    email = req.body.email,
+    token = req.body.token;
+  let checksql = "select * from comments where id = $1";
+  let updatesql = "update comments set comment=$1 where id =$2 returning *";
+  admin
+    .auth()
+    .verifyIdToken(token)
+    .then(() => {
+      return Connection.query(checksql, [commentId]).then((result) => {
+        if (result.rowCount > 0) {
+          if (result.rows[0].email !== email) {
+            return "You are not the Author of this comment!";
+          }
+          return Connection.query(updatesql, [comment, commentId]);
+        } else {
+          return null;
+        }
+      });
+    })
+    .then((result) => {
+      result
+        ? result.rows
+          ? res.status(200).json(result.rows)
+          : res.status(200).json({ message: result })
+        : res.status(404).json({ message: "There is no such comment!" });
+    })
+    .catch((e) => {
+      if (e) {
+        console.log(e.stack);
+        next(e);
+      } else {
+        res.send({ message: "Could not authorize" }).status(403);
+      }
+    });
+});
+
+router.delete("/comment", (req, res, next) => {
+  let commentId = req.body.id,
+    email = req.body.email,
+    token = req.body.token;
+  let checksql = "select * from comments where id = $1";
+  let deletesql = "delete from comments where id =$1 returning id";
+  admin
+    .auth()
+    .verifyIdToken(token)
+    .then(() => {
+      return Connection.query(checksql, [commentId]).then((result) => {
+        if (result.rowCount > 0) {
+          if (result.rows[0].email !== email) {
+            return "You are not the author of this comment!";
+          }
+          return Connection.query(deletesql, [commentId]);
+        } else {
+          return null;
+        }
+      });
+    })
+    .then((result) => {
+      result
+        ? result.rows
+          ? res.status(200).json(result.rows)
+          : res.status(200).json({ message: result })
+        : res.status(404).json({ message: "There is no such comment!" });
+    })
+    .catch((e) => {
+      if (e) {
+        console.log(e.stack);
+        next(e);
+      } else {
+        res.send({ message: "Could not authorize" }).status(403);
+      }
+    });
+});
+
+router.delete("/question", (req, res, next) => {
+  let questionId = req.body.id,
+    email = req.body.email,
+    token = req.body.token;
+  let checksql = "select * from questions where id = $1";
+  let deletesql = "delete from questions where id =$1 returning id";
+  admin
+    .auth()
+    .verifyIdToken(token)
+    .then(() => {
+      return Connection.query(checksql, [questionId]).then((result) => {
+        if (result.rowCount > 0) {
+          if (result.rows[0].email !== email) {
+            return "You are not the author of this question!";
+          }
+          return Connection.query(deletesql, [questionId]);
+        } else {
+          return null;
+        }
+      });
+    })
+    .then((result) => {
+      result
+        ? result.rows
+          ? res.status(200).json(result.rows)
+          : res.status(200).json({ message: result })
+        : res.status(404).json({ message: "There is no such question!" });
+    })
+    .catch((e) => {
+      if (e) {
+        console.log(e.stack);
+        next(e);
+      } else {
+        res.send({ message: "Could not authorize" }).status(403);
+      }
+    });
+});
 export default router;
