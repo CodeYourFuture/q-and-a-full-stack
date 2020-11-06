9c90a99e9427c69dc14c1f188269234d5726520c
Laetitia <laetitiachira@gmail.com>
2020-11-06 08:21:20 +0000

    added screenshot hint

--- client/src/Components/Editor.js
+++ client/src/Components/Editor.js
@@ -4,28 +4,37 @@ import "../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 const Editor = ({ editorState, onEditorStateChange }) => {
   return (
-    <DraftEditor
-      editorStyle={{ minHeight: "8em" }}
-      editorState={editorState}
-      toolbarClassName="text-dark "
-      wrapperClassName="text-dark"
-      editorClassName="text-dark"
-      onEditorStateChange={onEditorStateChange}
-      toolbar={{
-        options: ["inline", "list", "colorPicker", "link", "emoji", "image"],
-        inline: { inDropdown: false, options: ["monospace"] },
-        list: { inDropdown: true },
-        textAlign: { inDropdown: true },
-        link: { inDropdown: true },
-        history: { inDropdown: true },
+    <>
+      <DraftEditor
+        editorStyle={{ minHeight: "8em" }}
+        editorState={editorState}
+        toolbarClassName="text-dark "
+        wrapperClassName="text-dark"
+        editorClassName="text-dark"
+        onEditorStateChange={onEditorStateChange}
+        toolbar={{
+          options: ["inline", "list", "colorPicker", "link", "emoji", "image"],
+          inline: { inDropdown: false, options: ["monospace"] },
+          list: { inDropdown: true },
+          textAlign: { inDropdown: true },
+          link: { inDropdown: true },
+          history: { inDropdown: true },
 
-        image: {
-          defaultSize: {
-            width: "100%",
+          image: {
+            defaultSize: {
+              width: "100%",
+            },
           },
-        },
-      }}
-    />
+        }}
+      />
+      <p className="text-muted mt-2 border rounded px-1">
+        Hint: host your screenshots on &nbsp;
+        <a className="hint-link" href="https://linkpicture.com/en/?set=en">
+          LinkPicture,
+        </a>
+        &nbsp; click on image icon and share the URL.
+      </p>
+    </>
   );
 };
 
